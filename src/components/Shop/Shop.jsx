c08741a581f397427d35d078519ec30e47bcc322
unknown <shamiullipu@gmail.com>
2023-03-27 08:56:27 +0600

    data and image file path changed

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className='produnt-container'>
+                <h4>Product:</h4>
+            </div>
+            <div className='cart-container'>
+                <h4>Cart</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
